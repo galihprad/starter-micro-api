b97806f8a734409bd8ebc7ad7cff2720c1ea96aa
galihprad <galih.p.w.j@gmail.com>
2022-08-24 13:58:26 +0700

    refine

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-// var http = require('http');
-// http.createServer(function (req, res) {
-//     console.log(`Just got a request at ${req.url}!`)
-//     res.write('Yo!');
-//     res.end();
-// }).listen(process.env.PORT || 3000);
+var http = require("http");
+http
+  .createServer(function (req, res) {
+    console.log(`Just got a request at ${req.url}!`);
+    res.write("Yo!");
+    res.end();
+  })
+  .listen(process.env.PORT || 3000);
 
 const { Telegraf } = require("telegraf");
 
