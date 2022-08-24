549d8c843451a3b5d409abc4b365dd5f8dad39e6
galihprad <galih.p.w.j@gmail.com>
2022-08-24 13:35:49 +0700

    add telegraf

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+// var http = require('http');
+// http.createServer(function (req, res) {
+//     console.log(`Just got a request at ${req.url}!`)
+//     res.write('Yo!');
+//     res.end();
+// }).listen(process.env.PORT || 3000);
+
+const { Telegraf } = require("telegraf");
+
+const bot = new Telegraf("5793524297:AAHrdBqsfFAZU95RScWVRghlslZIu9eW4Ow");
+bot.start((ctx) => ctx.reply("Welcome"));
+bot.help((ctx) => ctx.reply("Send me a sticker"));
+bot.on("sticker", (ctx) => ctx.reply("👍"));
+bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+bot.launch();
